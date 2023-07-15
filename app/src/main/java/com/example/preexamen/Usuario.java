f32c8bbf3e3aeb40f1bb7d29012b61a8d0dd6677
JRSALCIDO <salcido.12311@gmail.com>
2023-07-15 14:27:37 -0700

    agragacion clase Usuario y UsuarioItem

---
+++ app/src/main/java/com/example/preexamen/Usuario.java
@@ -0,0 +1,54 @@
+package com.example.preexamen;
+
+import java.io.Serializable;
+
+public class Usuario implements Serializable {
+    private int id;
+    private String nombreUsuario;
+    private String correo;
+    private String contrasena;
+
+    public Usuario(int id, String nombreUsuario, String correo, String contrasena) {
+        this.nombreUsuario = "";
+        this.correo = "";
+        this.contrasena = "";
+    }
+
+    public Usuario(String nombreUsuario, String correo, String contrasena) {
+        this.nombreUsuario = nombreUsuario;
+        this.correo = correo;
+        this.contrasena = contrasena;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+}
